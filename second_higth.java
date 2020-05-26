8710bb814b5bd638bc665c86b011cebe901483fe
RajatVarshney24 <46565532+RajatVarshney24@users.noreply.github.com>
2020-05-26 21:02:47 +0530

    final commit

---
+++ second_higth.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class second_higth
+{
+public static  void main (String argp[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the size of array");
+int n =sc.nextInt();
+int[]arr =new int[n];
+for(int i = 0;i<n;i++)
+{
+int a[] =sc.nextArray[i];
+}
+//int l=int[i];/
+//int sl=int[];
+//System.out.println(l);
+//System.out.println(sl);
+}
+}
